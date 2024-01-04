aecb4b51ca5c483136c6687d30b80c19a4cad3cf
scsonic <scsonic@gmail.com>
2024-01-04 17:04:43 +0800

    Moving files to raw dir

--- example/android/app/src/main/java/com/hdb/avatar/ModelHelper.java
+++ example/android/app/src/main/java/com/hdb/avatar/ModelHelper.java
@@ -1,10 +1,17 @@
 package com.hdb.avatar;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Environment;
 import android.util.Log;
 
+import com.rnllamaexample.R;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,5 +50,42 @@ public class ModelHelper {
     }
 
     return glbFiles;
+  }
+
+  public static String copyRawFileToCache(Context context, int raw_id, String rawFileName) {
+    try {
+      // 打开 raw 资源文件的输入流
+      Resources resources = context.getResources();
+      InputStream inputStream = resources.openRawResource(raw_id);
+
+      // 创建缓存目录
+      //File cacheDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+      File cacheDir = context.getCacheDir();
+      File outputFile = new File(cacheDir, rawFileName);
+
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) {
+          outputStream.write(buffer, 0, length);
+        }
+      outputStream.close();
+
+
+      File jsonFile = new File(cacheDir, rawFileName.replace(".glb", ".json"));
+
+      FileOutputStream json_outputStream = new FileOutputStream(jsonFile);
+      String json_content = "{\"bodyType\":\"fullbody\",\"outfitGender\":\"feminine\",\"outfitVersion\":2,\"skinTone\":\"#ffd4b5\",\"createdAt\":\"2023-12-05T08:34:51.651Z\",\"updatedAt\":\"2023-12-18T11:48:24.499Z\"}";
+      byte[] jb = json_content.getBytes();
+      json_outputStream.write(jb, 0, jb.length);
+      json_outputStream.close();
+
+
+
+        return outputFile.getAbsolutePath();
+    } catch (IOException e) {
+      Log.e(TAG, "Copy file fail", e);
+    }
+    return null ;
   }
 }
